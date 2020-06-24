7f393760ebac2c4630176c3ded998d3f4fd780fa
oliver-little <oliver.little12@gmail.com>
2020-06-24 21:28:53 +0100

    Add basic code for bar lines

--- src/View/Timeline/Bar.ts
+++ src/View/Timeline/Bar.ts
@@ -1,21 +1,35 @@
 import * as PIXI from "pixi.js";
 
 export class BarTimeline extends PIXI.Container {
-    constructor() {
+
+    private _renderer : PIXI.Renderer;
+
+    constructor(renderer : PIXI.Renderer) {
         super();
+        this._renderer = renderer;
         let bar = new Bar();
-        bar.initialise();
+        bar.initialise(this._renderer, 5);
         this.addChild(bar);
     }
 }
 
-class Bar extends PIXI.Container  {
+class Bar extends PIXI.Container {
+
+    public static barWidth = 50;
+
+    private _graphics : PIXI.Graphics;
 
     constructor() {
         super();
+        this._graphics = new PIXI.Graphics();
+        this.addChild(this._graphics);
     }
 
-    public initialise() {
-        
+    public initialise(renderer : PIXI.Renderer, startX : number) {
+        this._graphics.beginFill(0xFFFFFF);
+        for (let i = 0; i < 4; i++) {
+            this._graphics.drawRect(startX + (Bar.barWidth * i), 0, 2, renderer.height);
+        }
+        this._graphics.endFill();
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
-import {BarTimeline} from "./View/Timeline/Bar.js";
+import { BarTimeline } from "./View/Timeline/Bar.js";
 import * as PIXI from "pixi.js";
-let app = new PIXI.Application({width: window.innerWidth * 0.8, height: window.innerHeight * 0.8});
 
-document.body.appendChild(app.view);
+window.onload = function () {
+    let app = new PIXI.Application({ width: window.innerWidth * 0.8, height: window.innerHeight * 0.8 });
 
-app.stage.addChild(new BarTimeline());
+    document.body.appendChild(app.view);
+
+    app.stage.addChild(new BarTimeline(app.renderer));
+
+}
